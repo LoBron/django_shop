44516d0b840cc3bbf4f459ded63797b4ee2c2a41
LoBron <lobanok2793@gmail.com>
2022-02-18 15:36:44 +0300

    add Category

--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,16 @@
 from django.db import models
 
 # Create your models here.
-class 
+class Category(models.Model):
+    """Модель категории"""
+    name = models.CharField(
+        'Название',
+        max_length=30,
+        unique=True)
+    slug = models.SlugField(
+        max_length='30',
+        unique=True)
+
+    def __str__(self):
+        return self.name
+
