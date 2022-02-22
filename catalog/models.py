309693f475f2c0f486ea43dbc2abc580284ae189
LoBron <lobanok2793@gmail.com>
2022-02-22 16:20:33 +0300

    add catalog

--- catalog/models.py
+++ catalog/models.py
@@ -3,36 +3,35 @@ from django.db import models
 # Create your models here.
 # from photologue.models import Photo, Gallery
 
+class Section(models.Model):
+    """Модель раздела"""
+    name = models.CharField('Название раздела', max_length=30, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class Category(models.Model):
     """Модель категории"""
-    name = models.CharField('Название', max_length=30, unique=True)
+    section = models.ForeignKey(Section, verbose_name='Раздел', on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField('Название категории', max_length=30, unique=True)
     slug = models.SlugField(max_length=30, unique=True)
 
     def __str__(self):
         return self.name
 
-class Photo(models.Model):
-    """Фотографии товара"""
-    main_photo = models.ImageField("Главная фотография", upload_to='photos/%Y/%m/%d')
-    additional_photo_01 = models.ImageField("Доп фото 1", upload_to='photos/%Y/%m/%d', null=True, blank=True)
-    additional_photo_02 = models.ImageField("Доп фото 2", upload_to='photos/%Y/%m/%d', null=True, blank=True)
-    additional_photo_03 = models.ImageField("Доп фото 3", upload_to='photos/%Y/%m/%d', null=True, blank=True)
-
-    class Meta:
-        verbose_name = "Фотография"
-        verbose_name_plural = "Фотографии"
-
-
 class Product(models.Model):
     """Модель товара"""
     category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
     title = models.CharField('Название', max_length=50)
+    # slug = models.SlugField(max_length=30)
     description = models.TextField('Описание')
     price = models.FloatField('Цена', default=0)
-    slug = models.SlugField(max_length=30)
     availability = models.BooleanField('Наличие', default=True)
-    amount = models.IntegerField('Количество', default=0)
-    photos = models.ForeignKey(Photo, verbose_name='Фотографии', on_delete=models.SET_NULL, null=True)
+    amount = models.PositiveIntegerField('Количество', default=0)
+    main_photo = models.ImageField("Главная фотография", upload_to='photos/%Y/%m/%d')
+    additional_photo_01 = models.ImageField("Доп фото 1", upload_to='photos/%Y/%m/%d', null=True, blank=True)
+    additional_photo_02 = models.ImageField("Доп фото 2", upload_to='photos/%Y/%m/%d', null=True, blank=True)
+    additional_photo_03 = models.ImageField("Доп фото 3", upload_to='photos/%Y/%m/%d', null=True, blank=True)
 
     class Meta:
         verbose_name = "Товар"
@@ -40,3 +39,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+class Furniture_plugs(models.Model):
+    """Заглушки мебельные"""
+
--- website/settings.py
+++ website/settings.py
@@ -82,17 +82,22 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'db_02',
+#         'USER': 'postgres',
+#         'PASSWORD': 'cirelchuk1966',
+#         #'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db_02',
-        'USER': 'postgres',
-        'PASSWORD': 'cirelchuk1966',
-        #'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
