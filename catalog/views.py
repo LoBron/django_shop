80a1724a6cd3182a32eefc83e27ad6550eaea916
Andrey Lobanok <lobanok2793@gmail.com>
2022-03-19 01:33:50 +0300

    working on catalog 01

--- catalog/views.py
+++ catalog/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def show_all_items(request):
-    return render(request, 'news/news_home.html')
+    return render(request, 'catalog/catalog_home.html')
