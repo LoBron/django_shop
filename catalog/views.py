53bbb501862f8b94f81bb523ef6b5309bb655675
LoBron <lobanok2793@gmail.com>
2022-03-21 21:30:44 +0300

    working about catalog

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from catalog.models import *
+
+
 def show_all_items(request):
-    return render(request, 'catalog/catalog_home.html')
+    products = Product.objects.all()
+    data = {
+        'products': products,
+        'title': 'Каталог'
+    }
+    return render(request, 'catalog/catalog_home.html', data)
+
+def show_category(request):
+    pass
