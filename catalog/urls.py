e6e6d5412dc5eb859a5a5c95025d52c745a33da7
LoBron <lobanok2793@gmail.com>
2022-03-23 15:16:58 +0300

    working about catalog

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.show_all_items, name='catalog_home'),
+    path('', views.show_catalog, name='catalog_home'),
+    path('#', views.show_category, name='category'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -4,18 +4,23 @@ from django.shortcuts import render
 from catalog.models import *
 
 
-def show_all_items(request):
+def show_catalog(request):
     products = Product.objects.all()
+    categoryes = Category.objects.all().order_by('name')
     data = {
         'products': products,
-        'title': 'Каталог'
+        'categoryes': categoryes,
+        'title': 'Каталог',
+        'logo': 'Категории',
     }
     return render(request, 'catalog/catalog_home.html', data)
 
-# def show_category(request, catid):
-#     products = Product.objects.all()
-#     data = {
-#         'products': products,
-#         'title': 'Каталог'
-#     }
-#     return render(request, 'catalog/catalog_home.html', data)
+def show_category(request, cat_id):
+
+    products = Product.objects.all()
+    data = {
+        'products': products,
+        'title': '???Категориия???',
+        'logo': '???Категориия???',
+    }
+    return render(request, 'catalog/catalog_category.html', data)
